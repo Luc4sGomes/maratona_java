69d8c57ff644ed9eb57351d7f19dbcf00edf23b3
Lucas Gomes <lucasgominhaa448@gmail.com>
2023-09-18 01:04:40 -0300

    :heavy_check_mark: added initial commit

---
+++ src/main/java/org/example/maratonajava/introduction/Aula01TiposPrimitivos.java
@@ -0,0 +1,10 @@
+package org.example.maratonajava.introduction;
+
+public class Aula01TiposPrimitivos {
+    public static void main(String[] args) {
+        //tipos primitivos são: int, double, float, char, byte, short, long e boolean
+        int age = 21;
+        System.out.println("The age is "+age);
+
+    }
+}
