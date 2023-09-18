954163393275376165b945dfa154d0d55ef76d89
Lucas Gomes <lucasgominhaa448@gmail.com>
2023-09-18 12:03:09 -0300

    added study about primitives types

--- src/main/java/org/example/maratonajava/introduction/Aula01TiposPrimitivos.java
+++ src/main/java/org/example/maratonajava/introduction/Aula01TiposPrimitivos.java
@@ -3,8 +3,15 @@ package org.example.maratonajava.introduction;
 public class Aula01TiposPrimitivos {
     public static void main(String[] args) {
         //tipos primitivos são: int, double, float, char, byte, short, long e boolean
-        int age = 21;
-        System.out.println("The age is "+age);
+        int age = 22;
+        double salaryDouble = 2000;
+        float salaryFloat = 2500;
+        byte ageByte = 1;
+        short ageShort = 10;
+        boolean verifyAge = false;
+        long bigNumber = 1000000;
+        char caracter = 'L';
 
+        System.out.println("The age is "+age);
     }
 }
