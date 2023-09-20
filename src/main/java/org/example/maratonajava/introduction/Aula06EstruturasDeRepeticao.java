1461873bff3dad8091b011a06dd1637462c01167
Lucas Gomes <lucasgominhaa448@gmail.com>
2023-09-19 21:32:12 -0300

    :sparkles: added revision about for, while and do while

---
+++ src/main/java/org/example/maratonajava/introduction/Aula06EstruturasDeRepeticao.java
@@ -0,0 +1,25 @@
+package org.example.maratonajava.introduction;
+
+public class Aula06EstruturasDeRepeticao {
+    public static void main(String[] args) {
+        //while, do while e for, podemos usar para iterar sobre alguma coisa, por exemplo, fazer algo conta te 10;
+        int count = 0;
+        while (count <= 10) {
+            count++;
+            System.out.println(count);
+        }
+
+
+        //executa pelo menos uma vez
+        do {
+            System.out.println("dentro do do while");
+        }
+        while(count <= 10);
+
+
+        //for
+        for(int variableIncrement = 0; variableIncrement < 10; variableIncrement++){
+            System.out.println("incrementing... "+variableIncrement);
+        }
+    }
+}
