44609671d321a38be7ecf033314262fe6c477d21
Lucas Gomes <lucasgominhaa448@gmail.com>
2023-11-07 23:47:37 -0300

    study about  interfaces

--- src/main/java/org/example/maratonajava/javacore/MInterfaces/domain/DataLoader.java
+++ src/main/java/org/example/maratonajava/javacore/MInterfaces/domain/DataLoader.java
@@ -2,7 +2,10 @@ package org.example.maratonajava.javacore.MInterfaces.domain;
 
 public interface DataLoader{
 
+    public static final int MAX_DATA_SIZE = 10;
+
     void load();
+
     default void isPermisson(){
         System.out.println("fazendo checagem de permissoes");
     };
