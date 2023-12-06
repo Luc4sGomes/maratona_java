3ef9815d41bb09b6090c4aaf1f3825857b5196e4
Lucas Gomes <lucasgominhaa448@gmail.com>
2023-12-06 17:36:11 -0300

    initial study using the book java8

---
+++ src/main/java/org/example/maratonajava/Java8Book/Cap2/Welcome1.java
@@ -0,0 +1,12 @@
+package org.example.maratonajava.Java8Book.Cap2;
+
+public class Welcome1 {
+    public static void main(String[] args) {
+        System.out.println("Welcome to java!");
+    }
+
+
+    /*
+
+     */
+}
